75ab33f0810ad67df677bf6246ef1877a71f1495
surafel27 <surafelfekadu399@gmail.com>
2023-01-25 05:08:58 -0800

    counting array sort 2nd

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -34,17 +34,11 @@ void counting_sort(int *array, size_t size)
 		return;
 	count = malloc(sizeof(int) * k + 1);
 	for (i = 0; i <= k; i++)
-	{
 		count[i] = 0;
-	}
 	for (i = 0; i < size; i++)
-	{
 		count[array[i]] = count[array[i]] + 1;
-	}
 	for (j = 1; j <= k; j++)
-	{
 		count[j] = count[j] + count[j - 1];
-	}
 	for (j = 1; j <= k; j++)
 	{
 		if (j == k)
@@ -62,9 +56,7 @@ void counting_sort(int *array, size_t size)
 		count[array[x]]--;
 	}
 	for (z = 0; z < size; z++)
-	{
 		array[z] = newarray[z];
-	}
 	free(count);
 	free(newarray);
 }
